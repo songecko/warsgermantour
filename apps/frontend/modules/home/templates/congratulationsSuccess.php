6f0ef93f9255a14b562e06daf1850ddffe0d278e
Diego D'amico <songecko@gmail.com>
2013-04-16 18:27:58 -0300

    -

--- apps/frontend/modules/home/templates/congratulationsSuccess.php
+++ apps/frontend/modules/home/templates/congratulationsSuccess.php
@@ -5,4 +5,5 @@
 	<a class="close" href="<?php echo url_for('homepage') ?>">cerrar</a>
     <h2>warsteiner german master tour</h2>
     <p>FELICITACIONES!! El código fué ingresado correctamente. <br />Estás dentro del avión.</p>
+    <p>Si se llena el avión te desplazarán de tu lugar y serás notificado para que puedas  volver a subirte y puedas ganar!</p>
 </div>
--- apps/frontend/modules/sfGuardAuth/templates/createProfileSuccess.php
+++ apps/frontend/modules/sfGuardAuth/templates/createProfileSuccess.php
@@ -10,7 +10,7 @@
 			A medida que nuevos participantes ingresen, irán desplazando a los demás. Si sos desplazado del avión, es importante que vuelvas a ingresar para poder participar!
 			Te esperan increíbles premios! un viaje a Alemania para 2 personas, heladeras Warsteiner, Giftpacks y muchos premios más!
 		</p>
-		<form class="form" method="post" action="<?php echo url_for('create_profile') ?>">
+		<form class="form" method="post" action="<?php echo url_for('create_profile') ?>" onsubmit="return validateForm();">
 			<?php echo $form['_csrf_token']->render() ?>
 			<?php echo $form['id']->render() ?>
 			<?php if(isset($form['user_id'])): ?>
@@ -39,3 +39,40 @@
 		</form>
 	</div>
 </div>
+<script type="text/javascript">
+var validateForm = function()
+{
+	if(!$('#sf_guard_user_profile_accept_bases').is(':checked'))
+	{
+		$(window).qtip({
+			id: "modal",
+			content: {
+				text:  'Para continuar debes aceptar las bases y condiciones',
+				title: {
+					text: 'Warsteiner - German Master Tour',
+					button: true
+				}
+			},
+			position: {
+				my: 'center',  // Position my top left...
+				at: 'center', // at the bottom right of...
+				target: $(window) // my target
+			},
+			show: {
+				event: false, // Don't specify a show event...
+				solo: true,
+				ready: true, // ... but show the tooltip when ready
+				modal: true
+			},
+			hide: false, // Don't specify a hide event either!
+			style: {
+				classes: 'ui-tooltip-red ui-tooltip-shadow'
+			}
+		});
+
+		return false;
+	}
+
+	return true;
+};
+</script>
